5e634e50b852e09b8513697f4e4917c9eeef3a2a
etsai <etsai2@gmail.com>
2014-01-02 14:32:41 -0800

    Checkpoint
    Started fixing runtime errors in the TSAdvanced reader class

--- data/TSAdvancedReader.groovy
+++ data/TSAdvancedReader.groovy
@@ -17,8 +17,8 @@ public class TSAdvancedReader implements DataReader {
         rowResult.remove("result")
         rowResult.remove("result_count")
 
-        if (!accum.contains(rowResult)) {
-            accum << rowResult
+        if (!accum.containsKey(rowResult)) {
+            accum[rowResult]= rowResult
         }
         switch(result) {
             case -1:
@@ -33,14 +33,14 @@ public class TSAdvancedReader implements DataReader {
        }
     }
     public Collection<Difficulty> getDifficulties() {
-        def diffStats= [] as Set
+        def diffStats= [:]
 
-        sql.eachRow('''select s.difficulty,s.length,m.result,sum(m.duration) as time,count(m.result) as result_count 
+        sql.eachRow('''select s.difficulty as name,s.length,m.result,sum(m.duration) as time,count(m.result) as result_count 
                 from match m inner join setting s on s.id=m.setting_id where m.result is not NULL 
                 group by s.difficulty,s.length,m.result;''') {
             resultAggregator(diffStats, it)
         }
-        diffStats.collect{ new Difficulty(it) }
+        diffStats.values().collect{ new Difficulty(it) }
     }
     public Collection<Level> getLevels() {
         def levelStats= [:]
@@ -49,14 +49,14 @@ public class TSAdvancedReader implements DataReader {
                 from match m inner join map m2 on m2.id=m.map_id where m.result is not NULL group by m2.name,m.result;''') {
             resultAggregator(levelStats, it)
         }
-        levelStats.collect{ new Level(it) }
+        levelStats.values().collect{ new Level(it) }
     }
     public PlayerRecord getRecord(String steamID64) {
-        def prValues= [disconnects: sql.firstRow("select count(*) from player_session where player_id=? and disconnected=true")[0], 
+        def prValues= [disconnects: sql.firstRow("select count(*) from player_session where player_id=? and disconnected=true", [steamID64])[0], 
                 finales_played: 0, finales_survived: 0, time: 0, wins: 0, losses: 0, steamid64: steamID64]
-        sql.eachRow('''select sum(finales_played) as fp_sum,sum(finales_survived) as fs_sum,sum(duration) as d_sum,result,
+        sql.eachRow('''select sum(finale_played::integer) as fp_sum,sum(finale_survived::integer) as fs_sum,sum(ps.duration) as d_sum,result,
                 count(m.result) as result_count from player_session ps inner join match m on m.id=ps.match_id where player_id=? 
-                and disconnected=false and m.result is not NULL group by m.result''', steamID64) {
+                and disconnected=false and m.result is not NULL group by m.result''', [steamID64]) {
             prValues.finales_played+= it.fp_sum
             prValues.finales_survived+= it.fs_sum
             prValues.time+= it.d_sum
@@ -79,14 +79,18 @@ public class TSAdvancedReader implements DataReader {
 
         def prValues= [:]
         sql.eachRow('''select player_id,count(*) from player_session ps inner join match m 
-                on m.id=ps.match_id where disconnected=true group by m.result,ps.player_id''' + orderStr + 
-                'limit ?, ?', [start, end - start]) {
+                on m.id=ps.match_id where disconnected=true group by m.result,ps.player_id ''' + orderStr + 
+                ' limit ? offset ?', [end - start, start]) {
             prValues[it.player_id]= [steamid64: it.player_id, disconnects: it[1], finales_played: 0, finales_survived: 0, 
                     time: 0, wins: 0, losses: 0]
         }
-        sql.eachRow('''select player_id,result,count(m.result) sum(finales_played) as fp_sum,sum(finales_survived) as fs_sum,sum(duration) as d_sum
+        sql.eachRow('''select player_id,result,count(m.result) as result_count, sum(finale_played::integer) as fp_sum,sum(finale_survived::integer) as fs_sum,sum(ps.duration) as d_sum
                 from player_session ps inner join match m on m.id=ps.match_id where disconnected=false and m.result is not NULL 
-                group by m.result,ps.player_id ''' + orderStr + 'limit ?, ?', [start, end - start]) {
+                group by m.result,ps.player_id ''' + orderStr + ' limit ? offset ?', [end - start, start]) {
+            if (prValues[it.player_id] == null) {
+                prValues[it.player_id]= [steamid64: it.player_id, disconnects: 0, finales_played: 0, finales_survived: 0, 
+                    time: 0, wins: 0, losses: 0]
+            }
             prValues[it.player_id].finales_played+= it.fp_sum
             prValues[it.player_id].finales_survived+= it.fs_sum
             prValues[it.player_id].time+= it.d_sum
@@ -102,7 +106,7 @@ public class TSAdvancedReader implements DataReader {
                     throw new Exception("Unrecognized result value: $result")
             }
         }
-        prValues.collect{ new PlayerRecord(it) }
+        prValues.values().collect{ new PlayerRecord(it) }
     }
     public Integer getNumRecords() {
         return sql.firstRow('select count(*) from player')[0]
@@ -116,6 +120,10 @@ public class TSAdvancedReader implements DataReader {
         }
         sql.eachRow('''select player_id,count(*) from player_session ps inner join match m 
                 on m.id=ps.match_id where disconnected=true group by m.result,ps.player_id''') {
+            if (prValues[it.player_id] == null) {
+                prValues[it.player_id]= [steamid64: it.player_id, disconnects: 0, finales_played: 0, finales_survived: 0, 
+                    time: 0, wins: 0, losses: 0]
+            }
             prValues[it.player_id].finales_played+= it.fp_sum
             prValues[it.player_id].finales_survived+= it.fs_sum
             prValues[it.player_id].time+= it.d_sum
@@ -139,7 +147,7 @@ public class TSAdvancedReader implements DataReader {
         sql.eachRow('''select s.difficulty,s.length,m2.name as level,m.result,ps.duration,
                 ps.timestamp,ps.wave from player_session ps inner join match m on m.id=ps.match_id 
                 inner join setting s on m.setting_id=s.id inner join map m2 on m2.id=m.map_id 
-                where player_id=? and m.result is not NULL''' + orderStr + ' LIMIT ?, ?', [steamID64, start, end - start]) {
+                where player_id=? and m.result is not NULL''' + orderStr + ' LIMIT ? OFFSET ?', [steamID64, end - start, start]) {
             matches << new Match(it.toRowResult())
         }
         return matches
@@ -147,7 +155,8 @@ public class TSAdvancedReader implements DataReader {
     }
     public Collection<Match> getMatchHistory(String steamID64) {
         def matches= []
-        sql.eachRow("""select s.difficulty,s.length,m2.name as map_name,m.result,ps.duration,
+        ///< TODO: match history result needs to be win,loss,or disconnect
+        sql.eachRow("""select s.difficulty,s.length,m2.name as level,m.result,ps.duration,
                 ps.timestamp,ps.wave from player_session ps inner join match m on m.id=ps.match_id 
                 inner join setting s on m.setting_id=s.id inner join map m2 on m2.id=m.map_id 
                 where player_id=$steamID64 and m.result is not NULL""") {
@@ -164,14 +173,14 @@ public class TSAdvancedReader implements DataReader {
 
         sql.eachRow('''select c.name from player_session ps1 inner join player_statistic ps2 on ps2.player_session_id=ps1.id 
                 inner join statistic s on s.id=ps2.statistic_id inner join category c on c.id=s.category_id group by c.name''') {
-            categories << c.name
+            categories << it.name
         }
         return categories
     }
     public Collection<Stat> getAggregateData(String category) {
         def stats= []
 
-        sql.eachRow("""select s.name,sum(ps.value) as value from player_statistic ps inner join statistic s on s.id=ps.statistic_id 
+        sql.eachRow("""select s.name as stat,sum(ps.value) as value from player_statistic ps inner join statistic s on s.id=ps.statistic_id 
                 where s.category_id in (select id from category where name=$category) group by s.name""") {
             stats << new Stat(it.toRowResult())
         }
@@ -187,8 +196,8 @@ public class TSAdvancedReader implements DataReader {
         return stats
     }
     public SteamIDInfo getSteamIDInfo(String steamID64) {
-        def info= sql.firstRow("select * from player where id=$steamID64")
-        return new SteamIDInfo(steamid64: info.id, name: info.name, avatar: info.avatar)
+        def info= sql.firstRow("select name,avatar from player where id=$steamID64")
+        return new SteamIDInfo(info)
     }
     public Collection<WaveStat> getWaveData(String difficulty, String length, String category) {
         def waveStats= []
@@ -226,23 +235,23 @@ public class TSAdvancedReader implements DataReader {
         return categories
     }
     public Collection<LevelDifficulty> getLevelData(String level) {
-        def levelData= [] as Set
+        def levelData= [:]
 
         sql.eachRow("""select s.difficulty,s.length,sum(m.wave) as wave_sum,count(m.result) as result_count,sum(m.duration) as time,m.result from match m 
                 inner join setting s on s.id=m.setting_id where m.map_id=(select id from map m2 where m2.name=$level) and 
                 m.result is not NULL group by s.difficulty,s.length,m.result""") {
             resultAggregator(levelData, it)
         }
-        levelData.collect{ new LevelDifficulty(it) }
+        levelData.values().collect{ new LevelDifficulty(it) }
     }
     public Collection<LevelDifficulty> getDifficultyData(String difficulty, String length) {
-        def diffData= [] as Set
-        sql.eachRow("""select m2.name,sum(m.wave),count(m.result) as result_count,sum(m.duration) as time,m.result from match m 
-                inner join map m2 on m2.id=m.map_id where m.setting_id=(select id from setting s where s.difficulty=$diffiulty and s.length=$length) and 
+        def diffData= [:]
+        sql.eachRow("""select m2.name as level,sum(m.wave) as wave_sum,count(m.result) as result_count,sum(m.duration) as time,m.result from match m 
+                inner join map m2 on m2.id=m.map_id where m.setting_id=(select id from setting s where s.difficulty=$difficulty and s.length=$length) and 
                 m.result is not NULL group by m2.name,m.result""") {
             resultAggregator(diffData, it)
         }
-        diffData.collect{ new LevelDifficulty(it) }
+        diffData.values().collect{ new LevelDifficulty(it) }
     }
     public Object getData(String queryName, List<Object> parameters) {
         throw new UnsupportedOperationException("Not yet implemented")
