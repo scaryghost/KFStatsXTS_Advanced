b9d487b2075707f6cb173f6b66a6d9e76fd3b1f9
etsai <etsai2@gmail.com>
2014-02-06 01:58:48 -0800

    Replaced old Match class with former HeatMap class

--- http/groovy/Match.groovy
+++ http/groovy/Match.groovy
@@ -2,183 +2,156 @@ import com.github.etsai.kfsxtrackingserver.web.Resource
 import groovy.xml.MarkupBuilder
 import java.util.UUID
 
-public class Index extends Resource {
-    private def resultNames= [(-1): "Loss", (0): "Map Voted", (1): "Win"]
-
+public class Match extends Resource {
     public String generatePage() {
         def matchUUID= UUID.fromString(queries.matchuuid)
         def writer= new StringWriter()
         def htmlBuilder= new MarkupBuilder(writer)
-        def waveData= reader.executeQuery("server_match_wave_data", matchUUID)
-        def wavePerks= reader.executeQuery("server_match_wave_perks", matchUUID)
-        def waves= wavePerks.collect(new TreeSet()){ it.wave }
 
         htmlBuilder.html() {
             head() {
                 meta('http-equiv':"Content-Type", content:"text/html; charset=utf-8")
-                title('Flot Examples: AJAX')
-                style('td+td { text-align: right;}')
-                link(href:'css/examples.css', rel:'stylesheet', type:'text/css')
-                script(language:"javascript", type:"text/javascript", src:"js/jquery.js", '')
-                script(language:"javascript", type:"text/javascript", src:"js/jquery.flot.js", '')
-                script(language:"javascript") {
+                title('Bubble Chart Example')
+                script(type:'text/javascript', src:'//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', '')
+                script(type:'text/javascript') {
                     mkp.yieldUnescaped """
-    \$(function() {
-        var options = {
-            lines: {
-                show: true
-            },
-            points: {
-                show: true
-            },
-            grid: {
-				hoverable: true,
-				clickable: true
-			},
-            xaxis: {
-                tickDecimals: 0,
-                tickSize: 1
-            }
-        };
-        function onDataReceived(series) {
-                \$.plot("#placeholder", [series], options);
-            }
+    function colorCells(tableId) {
+        Array.max = function( array ){
+        return Math.max.apply( Math, array );
+    };
 
-            \$.ajax({
-                    url: "data.json?matchuuid=$matchUUID",
-                    type: "GET",
-                    dataType: "json",
-                    success: onDataReceived
-                });
-    \$("<div id='tooltip'></div>").css({
-			position: "absolute",
-			display: "none",
-			border: "1px solid #fdd",
-			padding: "2px",
-			"background-color": "#fee",
-			opacity: 0.80
-		}).appendTo("body");
-        \$("#placeholder").bind("plothover", function (event, pos, item) {
-				if (item) {
-					var x = item.datapoint[0].toFixed(2),
-						y = item.datapoint[1].toFixed(2);
+    var fullTableId= '#' + tableId + ' tbody td';
+    // get all values
+    var counts= \$(fullTableId).not('.stats-title').map(function() {
+        return parseInt(\$(this).text());
+    }).get();
+	
+	// return max value
+	var max = Array.max(counts);
+	
+	xr = 255;
+    xg = 255;
+    xb = 255;
+   
+    yr = 243;
+    yg = 32;
+    yb = 117;
 
-					\$("#tooltip").html(y + "s")
-						.css({top: item.pageY+5, left: item.pageX+5})
-						.fadeIn(200);
-				} else {
-					\$("#tooltip").hide();
-				}
-		});
-      });
+    n = 100;
+	
+	// add classes to cells based on nearest 10 value
+	\$(fullTableId).not('.stats-title').each(function(){
+		var val = parseInt(\$(this).text());
+		var pos = parseInt((Math.round((val/max)*100)).toFixed(0));
+		red = parseInt((xr + (( pos * (yr - xr)) / (n-1))).toFixed(0));
+		green = parseInt((xg + (( pos * (yg - xg)) / (n-1))).toFixed(0));
+		blue = parseInt((xb + (( pos * (yb - xb)) / (n-1))).toFixed(0));
+		clr = 'rgb('+red+','+green+','+blue+')';
+		\$(this).css({backgroundColor:clr});
+	});
+    }
+    \$(document).ready(function(){
+	// Function to get the Max value in Array
+        Array.prototype.forEach.call(document.getElementsByClassName('heat-map'), function(entry) {
+            colorCells(entry.getAttribute('id'));
+        });
+});"""
+                }
+                style(type:'text/css') {
+                    mkp.yieldUnescaped """
+    body,html,div,blockquote,img,label,p,h1,h2,h3,h4,h5,h6,pre,ul,ol,li,dl,dt,dd,form,a,fieldset,input,th,td{border:0;outline:none;margin:0;padding:0;}
+body{height:100%;background:#fff;color:#1f1f1f;font-family:Arial,Verdana,sans-serif;font-size:13px;padding:7px 0;}
+ul, ol{list-style:none;}
+a {text-decoration: none;}
+.text-center {text-align: center; padding: 10px 0;}
+h2 {margin-bottom: 30px;}
+.wrap {width: 653px; margin: 0 auto;}
+.clear {clear: both;}
+
+/* Tutorial CSS */
+.heat-map {border: 1px solid #ccc; margin: 0 auto 20px auto; width: 650px;}
+.heat-map tr th {padding: 5px 7px; border-right: 1px solid #ccc; border-bottom: 1px solid #ccc; border-top: 1px solid #fff; border-left: 1px solid #fff; text-shadow: 0 1px 0 #fff; font: bold 11px Arial, sans-serif; text-transform: uppercase; color: #000;
+background-color: #F1F1F1;
+    background-image: -moz-linear-gradient(center top , #F9F9F9, #ECECEC);
+}
+.heat-map tr th.last {border-right: none;}
+.heat-map tr th.first, .heat-map tr td.stats-title {border-left: none; text-align: left;}
+.heat-map tr td {padding: 5px 7px; /*border-top: 1px solid #fff; border-right: 1px solid #fff; */ border-right: 1px solid #fff;border-bottom: 1px solid #fff;text-align right; color: #000; font-size: 11px;}
+tr.stats-row {text-align: right;}
+.heat-map tr.stats-row td.stats-title {text-align: left; color: #111; font-size: 13px; text-shadow: 0 1px 0 #fff; background: #efefef; border-top: 1px solid #fff; border-bottom: 1px solid #ccc;  border-right: 1px solid #ccc;}
+.heat-map tr.stats-row td.stats-title.last {border-bottom: 1px solid #fff;}
 """
                 }
             }
             body() {
-                div(id:'header') {
-                    h2('Match Information')
+                def wavePerks= reader.executeQuery("server_match_wave_perks", matchUUID)
+                def waveSummaries= reader.executeQuery("server_wave_summaries", matchUUID)
+                def data= reader.executeQuery("server_match_wave_data", matchUUID)
+                def waves= wavePerks.collect(new TreeSet()){ it.wave }
+                def index= -1
+
+                def summary= reader.executeQuery("server_match", matchUUID)
+                table(class:'heat-map') {
+                    tbody() {
+                        summary.each{key, value ->
+                            tr(class: 'stats-row') {
+                                td(class: 'stats-title', key)
+                                td(value)
+                            }
+                        }
+                    }
                 }
-                div(id:'content') {
-                    div(style: 'float: left; width: 50%;') {
-                        def summary= reader.executeQuery("server_match", matchUUID)
-                        table() {
+                waves.each {waveNum ->
+                    def perks= wavePerks.findAll{ it.wave == waveNum }
+                    h2(class:'text-center', "Wave $waveNum")
+                    table(class:'heat-map') {
+                        tobdy() {
+                            tr(class: 'stats-row') {
+                                td(class:'stats-title', rowspan: 2, 'Perks')
+                                perks.each{ perk ->
+                                    td(perk.name)
+                                }
+                            }
+                                tr(class: 'stats-row') {
+                                perks.each{ perk ->
+                                    td(perk.count)
+                                }
+                            }
+                            waveSummaries.find{ it.wave == waveNum }.each {key, val ->
+                                tr(class: 'stats-row') {
+                                    td(class:'stats-title', key)
+                                    td(colspan: perks.size() - 1 ,val)
+                                }
+                            }
+                        }
+                    }
+                    data.collect(new HashSet()){ it.category  }.each {category->
+                        table(cellpadding:"0", cellspacing:"0", border:"0", class:"heat-map", id:"heat-map-${(++index)}") {
+                            thead() {
+                                tr(class: 'stats-row') {
+                                    th(class:'first', category)
+                                    wavePerks.findAll{ it.wave == waveNum}.each {perk ->
+                                        th(perk.name)
+                                    }
+                                }
+                            }
                             tbody() {
-                                tr() {
-                                    td('Number of Players')
-                                    td(summary.num_players)
-                                }
-                                tr() {
-                                    td('Server IP')
-                                    td("${summary.address}:${summary.port}")
-                                }
-                                tr() {
-                                    td('Difficulty')
-                                    td(summary.difficulty)
-                                }
-                                tr() {
-                                    td('Length')
-                                    td(summary.length)
-                                }
-                                tr() {
-                                    td('Map')
-                                    td(summary.name)
-                                }
-                                tr() {
-                                    td('Wave')
-                                    td(summary.wave)
-                                }
-                                tr() {
-                                    td('Result')
-                                    td(resultNames[summary.result])
+                                data.findAll{it.category == category && it.wave == waveNum }.collect(new HashSet()){ it.stat }.each{ stat ->
+                                    tr(class:'stats-row') {
+                                        td(class:'stats-title', stat)
+                                        wavePerks.findAll{ it.wave == waveNum }.each {perk ->
+                                            def result= data.find{it.stat == stat && it.perk == perk.name && it.category == category && it.wave == waveNum}
+                                            td(result == null ? 0 : result.value)
+                                        }
+                                    }
                                 }
                             }
                         }
-                    }
-                    div(class:"demo-container") {
-                        div(id:"placeholder",class:"demo-placeholder",'')
                     }
                 }
-                    def waveSummaries= reader.executeQuery("server_wave_summaries", matchUUID)
-                    waves.each {wave ->
-                        def perks= wavePerks.findAll{ it.wave == wave }
-                        def summary= waveSummaries.find{ it.wave == wave }
-                        def data= waveData.findAll{ it.wave == wave }
-
-                        div(id: "wave_section_$wave", style:"clear: right; width: 1024px") {
-                            table(style: 'width: 50%', border: 1) {
-                                tr() {
-                                    def head= perks.head()
-                                    td(rowspan: perks.size(), 'Perks')
-                                    td(head.name, style: "width: 50%")
-                                    td(head.count)
-                                }
-                                perks.tail().each {p ->
-                                    tr() {
-                                        td(p.name, style: "width: 50%")
-                                        td(p.count)
-                                    }
-                                }
-                                tr() {
-                                    td('Duration')
-                                    td(summary.duration)
-                                    td()
-                                }
-                                tr() {
-                                    td('Survived')
-                                    td(summary.survived)
-                                    td()
-                                }
-                            }
-                            data.collect(new HashSet()){ it.category }.each {category->
-                                table() {
-                                    thead() {
-                                        tr() {
-                                            th(category)
-                                            perks.each {perk ->
-                                                th(perk.name)
-                                            }
-                                        }
-                                    }
-                                    tbody() {
-                                        data.findAll{it.category == category}.collect(new HashSet()){ it.stat }.each {stat ->
-                                            tr() {
-                                                td(stat)
-                                                perks.each {perk ->
-
-                                                    def result= data.find{it.category == category && it.stat == stat && it.perk == perk.name}
-                                                    td(result == null ? 0 : result.value)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        hr()
-                    }
             }
         }
 
-        return String.format("<!DOCTYPE HTML>%n%s", writer)
+        return String.format("<!DOCTYPE HTML>%s%n", writer)
     }
 }
