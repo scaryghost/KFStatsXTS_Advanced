98929dd1729c78b6ad503461152041b3d8363aa7
etsai <etsai2@gmail.com>
2014-03-04 03:57:41 -0800

    Setup nav menu
    For now, menu will link to stats, servers, players, and matches

--- http/groovy/Summary.groovy
+++ http/groovy/Summary.groovy
@@ -5,6 +5,14 @@ public class Summary extends WebPage {
         jsSrcs << 'js/heatmap.js'
     }
     protected void fillBody(builder) {
+        builder.div(style: 'width: 650px;margin-left: auto;margin-right: auto;') {
+            ul(class:'nav-list') {
+                li(class:'nav-list', "stats")
+                li(class:'nav-list', "servers")
+                li(class:'nav-list', "players")
+                li(class:'nav-list', "matches")
+            }
+        }
         ["server_totals", "server_setting_stats", "server_map_stats"].each {query ->
             builder.table(cellpadding:"0", cellspacing:"0", border:"0", 
                     class:"heat-map") {
